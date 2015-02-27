32461798b9f1a9ea425f41d56dfe61700d3b3e73
Damir <damir@withpulp.com>
2015-02-27 16:49:13 -0500

    added disqus, removed browser-security due to issues with disqus

--- server/config/security.js
+++ server/config/security.js
@@ -1,2 +1,10 @@
-BrowserPolicy.content.allowOriginForAll("http://fonts.googleapis.com");
-BrowserPolicy.content.allowOriginForAll("http://fonts.gstatic.com");
+//BrowserPolicy.content.allowOriginForAll('http://fonts.googleapis.com');
+//BrowserPolicy.content.allowOriginForAll('http://fonts.gstatic.com');
+//BrowserPolicy.content.allowOriginForAll('*.google-analytics.com');
+//BrowserPolicy.content.allowOriginForAll('a.disquscdn.com');
+//BrowserPolicy.content.allowOriginForAll('http://undefined.disqus.com/embed.js');
+
+//BrowserPolicy.content.disallowInlineStyles();
+//BrowserPolicy.content.disallowInlineScripts();
+
+//currently breaks disqus
